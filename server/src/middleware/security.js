ac2b471b809ae7ff6b8e7d51b22140c14f93532b
Vansh Shende <vshende719@gmail.com>
2026-06-19 13:11:18 +0530

    security: whitelist production Vercel frontend URL in CORS

--- server/src/middleware/security.js
+++ server/src/middleware/security.js
@@ -39,6 +39,8 @@ const allowedOrigins = [
   'http://127.0.0.1:5173',
   'http://localhost:5174',
   'http://127.0.0.1:5174',
+  'https://client-eight-kohl.vercel.app',
+  config.cors.origin,
 ];
 
 const corsMiddleware = cors({
